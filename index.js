226e9e7b4e34667d6b070adc281ab395e53118f7
Willy Osorto <arwy1806@gmail.com>
2019-10-10 14:00:46 -0600

    Adding cors codding

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require('express')
 const port = process.env.PORT || 3000;
 var cors = require('cors')
 const app = express()
-app.use(cors())
+app.use(function(req, res, neext){
+    res.header("Access-Control-Allow-Origin", "https://portal.form.io/"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
 
 app.get('/programs', function (req, res) {
     res.send({
