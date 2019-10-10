15379cbc1aa5dfad44fa5b5a1f1cd17a1b9bf71b
Willy Osorto <arwy1806@gmail.com>
2019-10-09 23:56:57 -0600

    Adding programs, programcampuses, campuses, students, studentprograms endpoints

--- index.js
+++ index.js
@@ -1,24 +1,254 @@
 const express = require('express')
+const port = process.env.PORT || 3000;
 const app = express()
- 
-app.get('/', function (req, res) {
-  res.send({
-      'info': {
-          'name':'Arthur Fleck',
-          'programs':[
-              {
-                  'programName':'Letras',
-                  'programId':'LET_B1',
-                  'termCode':'201920'
-              },
-              {
-                  'programName':'Direito Bacharelado',
-                  'programId':'DCH_B1',
-                  'termCode':'201920'
-              }
-          ]
-      }
-  })
+
+app.get('/programs', function (req, res) {
+    res.send({
+        "data": [
+            {
+                "codigo": "019_B1",
+                "descricao": "Ciencia da Computacao",
+                "siglaIES": "FMU",
+            },
+            {
+                "codigo": "026_B1",
+                "descricao": "Ciencias Atuariais",
+                "siglaIES": "FMU",
+            },
+            {
+                "codigo": "093_B1",
+                "descricao": "Ciencias Biologicas",
+                "siglaIES": "FMU",
+            },
+            {
+                "codigo": "311_B1",
+                "descricao": "Ciencias Contabeis",
+                "siglaIES": "FIAMFA",
+            },
+            {
+                "codigo": "004_B1",
+                "descricao": "Ciencias Economicas",
+                "siglaIES": "FIAMFA",
+            }
+        ]
+    })
 })
- 
-app.listen(3001)
+
+app.get('/programcampuses', function (req, res) {
+    res.send({
+        "data": [
+            {
+                "programId": "019_B1",
+                "campuses": [
+                    {
+                        "campusId": "FA",
+                        "campusName": "Liberdade - Prédio 26",
+                    },
+                    {
+                        "campusId": "FB",
+                        "campusName": "Casa do Direito",
+                    }
+                ]
+            },
+            {
+                "programId": "026_B1",
+                "campuses": [
+                    {
+                        "campusId": "FC",
+                        "campusName": "Ibirapuera",
+                    },
+                    {
+                        "campusId": "FJ",
+                        "campusName": "Vila Mariana II",
+                    }
+                ]
+            },
+            {
+                "programId": "093_B1",
+                "campuses": [
+                    {
+                        "campusId": "ND",
+                        "campusName": "Nao definido",
+                    },
+                    {
+                        "campusId": "FI",
+                        "campusName": "Beneficência",
+                    },
+                ]
+            },
+            {
+                "programId": "311_B1",
+                "campuses": [
+                    {
+                        "campusId": "FL",
+                        "campusName": "Morumbi",
+                    },
+                    {
+                        "campusId": "FH",
+                        "campusName": "Vila Mariana I",
+                    },
+                ]
+            },
+            {
+                "programId": "004_B1",
+                "campuses": [
+                    {
+                        "campusId": "FM",
+                        "campusName": "Liberdade - Clínicas",
+                    },
+                    {
+                        "campusId": "FN",
+                        "campusName": "Ponte Estaiada / Hovet",
+                    },
+                ]
+            }
+        ]
+    })
+})
+
+app.get('/campuses', function (req, res) {
+    res.send({
+        "data": [
+            {
+                "campusId": "FA",
+                "campusName": "Liberdade - Prédio 26",
+                "shifts": ["Matutino", "Vespertino", "Nocturno"]
+            },
+            {
+                "campusId": "FB",
+                "campusName": "Casa do Direito",
+                "shifts": ["Matutino", "Vespertino", "Nocturno"]
+            },
+            {
+                "campusId": "FC",
+                "campusName": "Ibirapuera",
+                "shifts": ["Matutino", "Vespertino", "Nocturno"]
+            },
+            {
+                "campusId": "FJ",
+                "campusName": "Vila Mariana II",
+                "shifts": ["Matutino", "Vespertino", "Nocturno"]
+            },
+            {
+                "campusId": "ND",
+                "campusName": "Nao definido",
+                "shifts": ["Matutino", "Vespertino", "Nocturno"]
+            },
+            {
+                "campusId": "FI",
+                "campusName": "Beneficência",
+                "shifts": ["Matutino", "Vespertino", "Nocturno"]
+            },
+            {
+                "campusId": "FL",
+                "campusName": "Morumbi",
+                "shifts": ["Matutino", "Vespertino", "Nocturno"]
+            },
+            {
+                "campusId": "FH",
+                "campusName": "Vila Mariana I",
+                "shifts": ["Matutino", "Vespertino", "Nocturno"]
+            },
+            {
+                "campusId": "FM",
+                "campusName": "Liberdade - Clínicas",
+                "shifts": ["Matutino", "Vespertino", "Nocturno"]
+            },
+            {
+                "campusId": "FN",
+                "campusName": "Ponte Estaiada / Hovet",
+                "shifts": ["Matutino", "Vespertino", "Nocturno"]
+            },
+        ]
+    })
+})
+
+app.get('/students', function (req, res) {
+    res.send({
+        "data": [
+            {
+                "studentID": "A00010568",
+                "firstName": "Arthur",
+                "lastName": "Fleck",
+                "institution": "FMU",
+                "campus": "Liberdade - Prédio 26",
+            },
+            {
+                "studentID": "A00015087",
+                "firstName": "Diana",
+                "lastName": "Prince",
+                "institution": "FMU",
+                "campus": "Casa do Direito",
+            },
+            {
+                "studentID": "A00027918",
+                "firstName": "Steve",
+                "lastName": "Rogers",
+                "institution": "FIAMFA",
+                "campus": "Ponte Estaiada / Hovet",
+            },
+            {
+                "studentID": "A00027916",
+                "firstName": "Carol",
+                "lastName": "Danvers",
+                "institution": "FIAMFA",
+                "campus": "Ibirapuera",
+            },
+        ]
+    })
+})
+
+app.get('/studentprograms', function (req, res) {
+    res.send({
+        "data": [
+            {
+                "studentID": "A00010568",
+                "programs": [
+                    {
+                        "codigo": "019_B1",
+                        "descricao": "Ciencia da Computacao",
+                    },
+                    {
+                        "codigo": "026_B1",
+                        "descricao": "Ciencias Atuariais",
+                    }
+                ]
+            },
+            {
+                "studentID": "A00015087",
+                "programs": [
+                    {
+                        "codigo": "093_B1",
+                        "descricao": "Ciencias Biologicas",
+                    },
+                ]
+            },
+            {
+                "studentID": "A00027918",
+                "programs": [
+                    {
+                        "codigo": "311_B1",
+                        "descricao": "Ciencias Contabeis",
+                    }
+                ]
+            },
+            {
+                "studentID": "A00027916",
+                "programs": [
+                    {
+                        "codigo": "311_B1",
+                        "descricao": "Ciencias Contabeis",
+                    },
+                    {
+                        "codigo": "004_B1",
+                        "descricao": "Ciencias Economicas",
+                    }
+                ]
+            },
+        ]
+    })
+})
+
+app.listen(port, function () {
+    console.log(`App listening`)
+})
