3c0eab7798066129c533a8a496c6a32d38cc165a
Willy Osorto <arwy1806@gmail.com>
2019-10-10 15:32:37 -0600

    removing cors

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 const express = require('express')
 const port = process.env.PORT || 3000;
-var cors = require('cors')
-const app = express()
 
-app.use(cors())
-app.use(function(req, res, next){
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-})
+const app = express()
 
 app.get('/programs', function (req, res) {
     res.send({
