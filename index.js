42b8648ca4bbc874736f90d6be8d82a37c54c68b
Willy Osorto <arwy1806@gmail.com>
2019-10-10 14:03:42 -0600

    Fixing cors

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 var cors = require('cors')
 const app = express()
 app.use(function(req, res, neext){
-    res.header("Access-Control-Allow-Origin", "https://portal.form.io/"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 })
