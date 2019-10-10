a9da83b5b52491b32b7ef7c57e7480149f234153
Willy Osorto <arwy1806@gmail.com>
2019-10-10 14:19:14 -0600

    Adding cors and cotroll-allow

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const port = process.env.PORT || 3000;
-//var cors = require('cors')
+var cors = require('cors')
 const app = express()
 
-/* app.use(function(req, res, next){
+app.use(cors())
+app.use(function(req, res, next){
     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-}) */
+})
 
 app.get('/programs', function (req, res) {
     res.send({
