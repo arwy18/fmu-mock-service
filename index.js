db0246aedf36f2701f40c1788375f2e30bde6b5c
Willy Osorto <arwy1806@gmail.com>
2019-10-11 10:44:42 -0600

    allowing control access

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const port = process.env.PORT || 3000;
 
 const app = express()
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://portal.form.io");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/programs', function (req, res) {
     res.send({
         "data": [
